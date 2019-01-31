2af3e3e64750f66e166a87f005507e197721f408
Vipul Singhal <vipulsinghal02@gmail.com>
2019-02-01 01:52:05 +0800

    added parfor in the initial walker positions in gwmcmc_vse

--- mcmc_simbio/src/gwmcmc/gwmcmc_vse.m
+++ mcmc_simbio/src/gwmcmc/gwmcmc_vse.m
@@ -146,33 +146,33 @@ logP=nan(NPfun,Nwalkers,Nkeep);
 
 wix = 1;
 for fix=1:NPfun % function index
-        try
-            v=logPfuns{fix}(minit(:,wix));
-        catch ME
-            if strcmp(ME.identifier, 'DESuite:ODE15S:IntegrationToleranceNotMet')
-                v = -Inf; % could potentially change this to something like -50, but then 
-                % if here integration tolerances are not met, an error is
-                % thrown. So we NEED integration tolerances to be met
-                % initially. This is because of the way things are compared
-                % in the future iterations, and accepted and rejected.
-                % Should be -Inf, but I have changed it to -20. in the
-                % later iterations, keep it at -Inf, so that any future
-                % integration tol errors also get rejected. 
-            end
+    try
+        v=logPfuns{fix}(minit(:,wix));
+    catch ME
+        if strcmp(ME.identifier, 'DESuite:ODE15S:IntegrationToleranceNotMet')
+            v = -Inf; % could potentially change this to something like -50, but then
+            % if here integration tolerances are not met, an error is
+            % thrown. So we NEED integration tolerances to be met
+            % initially. This is because of the way things are compared
+            % in the future iterations, and accepted and rejected.
+            % Should be -Inf, but I have changed it to -20. in the
+            % later iterations, keep it at -Inf, so that any future
+            % integration tol errors also get rejected.
         end
-        if islogical(v)
-            %reformulate function so that false=-inf for logical constraints.
-            v=-1/v;logPfuns{fix}=@(m)-1/logPfuns{fix}(m);
-            %experimental implementation of experimental feature
-        end
-        logP(fix,wix,1)=v;
+    end
+    if islogical(v)
+        %reformulate function so that false=-inf for logical constraints.
+        v=-1/v;logPfuns{fix}=@(m)-1/logPfuns{fix}(m);
+        %experimental implementation of experimental feature
+    end
+    logP(fix,wix,1)=v;
 end
 
 
 
 
 
-for wix=2:Nwalkers % walker index
+parfor wix=2:Nwalkers % walker index
     for fix=1:NPfun % function index
         try
             logP(fix,wix,1)=logPfuns{fix}(minit(:,wix));
