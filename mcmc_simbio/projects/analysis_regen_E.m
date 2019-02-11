6ae07919651d24a2bb7a93311d2d2e4e4e90dcb7
Vipul Singhal <vipulsinghal02@gmail.com>
2019-02-11 21:47:10 +0800

    F1 is F with expanded allowable parameter ranges.

--- mcmc_simbio/projects/analysis_regen_E.m
+++ mcmc_simbio/projects/analysis_regen_E.m
@@ -1,6 +1,4 @@
 
-% UNDER CONSTRUCTION!!!! hold.
-
 % In this file we try to find a set of parameter values that allow the
 % model from model_dsg2014_regen to fit the data from data_dsg2014_full.
 % Vipul Singhal, 2019
@@ -11,6 +9,9 @@
 % Set working directory to the txtlsim toolbox directory.
 projdir = [pwd '/mcmc_simbio/projects/proj_acs_dsg2014_regen_E'];
 addpath(projdir)
+
+jpgsave = false;
+figsave = false;
 
 % Load model, mcmc_info, and data_info.
 mobj = model_dsg2014_regen;
@@ -27,8 +28,10 @@ plotflag = true;
 switch tsIDtouse
     case 1
         ts1 = '20190209_193419_1_22';
-        tstamp = {ts1};
-        nIterID = {1:12};
+        ts2 = '20190210_072219_1_20';
+        ts3 = '20190210_184717_1_2';
+        tstamp = {ts1 ts2 ts3};
+        nIterID = {1:14 1:10 1:8};
         load([projdir '/simdata_' ts1 '/full_variable_set_' ts1 '.mat'], ...
             'mi',...
             'mcmc_info', 'data_info', 'mai', 'ri');
@@ -38,7 +41,7 @@ mai.masterVector
 
 
 marray_full = mcmc_get_walkers(tstamp,nIterID, projdir);
-marray = marray_full(:,:,1500:end);
+marray = marray_full(:,:,1:end);
 parnames = ...
     [{'TX_{cat}'    }
     {'pol_{Kd}'     }
@@ -56,12 +59,16 @@ parnames = ...
 if plotflag
     close all
     % Plot trace and corner (posterior distribution) plots
-mcmc_plot(marray(:, 1:50:end,:), parnames(:));
-%     mcmc_plot(marray(1:6, 1:20:end,1:end), parnames(1:6));
-%     mcmc_plot(marray(6+(1:8), 1:20:end,1:end), parnames(6+(1:8)));
-
+%     mcmc_plot(marray(:, 1:3:end,1:50:end), parnames(:));
+    mcmc_plot(marray(:, 1:6:end,4000:10:end), parnames(:),...
+    'savematlabfig', figsave, 'savejpeg', jpgsave,...
+    'projdir', projdir, 'tstamp', ts1, 'extrafignamestring', 'BurnedIn');
+    
+    mcmc_plot(marray(:, 1:10:end,1:20:end), parnames(:),...
+    'savematlabfig', figsave, 'savejpeg', jpgsave,...
+    'projdir', projdir, 'tstamp', ts1, 'extrafignamestring', 'WithTransient');
     figure
-    [C,lags,ESS]=eacorr(marray(:, :,:));%10000:end
+    [C,lags,ESS]=eacorr(marray(:, :,4000:end));%10000:end
     plot(lags,C,'.-',lags([1 end]),[0 0],'k');
     grid on
     xlabel('lags')
@@ -69,26 +76,44 @@ mcmc_plot(marray(:, 1:50:end,:), parnames(:));
     text(lags(end),0,sprintf('Effective Sample Size (ESS): %.0f_ ',...
         ceil(mean(ESS))),'verticalalignment','bottom','horizontalalignment','right')
     title('Markov Chain Auto Correlation')
-    %
-    titls = arrayfun(@num2str, mi(1).dosedVals, 'UniformOutput', false);
-    titls_array = cell(length(titls), 1, length(mi(2).measuredSpeciesIndex));
-    for i = 1:length(mi(2).measuredSpeciesIndex)
-        for j = 1:length(titls)
-            titls_array(j, 1, i) = titls(j);
-        end
-    end
+    
+    
     mvarray = masterVecArray(marray, mai);
-    samplePoints = ceil(size(mvarray, 3) * [.8, 1]);
+    samplePoints = ceil(size(mvarray, 3) * [.9, 1]);
     %
     marrayOrd = mvarray(mi(1).paramMaps(mi(1).orderingIx),:,samplePoints);
+    titls = arrayfun(@num2str, mi(1).dosedVals, 'UniformOutput', false);
+    titls_array = cell(length(titls), 2, length(mi(1).measuredSpeciesIndex));
+    ms = {'RNA'};
+    for i = 1:length(mi(1).measuredSpeciesIndex)
+        for j = 1:length(titls)
+            titls_array(j, 1, i) = {[ms{i} ', ' titls{j} 'nM initial RNA, Exp data']};
+            titls_array(j, 2, i) = {[ms{i} ', ' titls{j} 'nM initial RNA, MCMC samples']};
+        end
+    end
     mcmc_trajectories(mi(1).emo, data_info(mi(1).dataToMapTo), mi(1), marrayOrd,...
-        titls', {},...
-        'SimMode', 'meanstd');%, 'savematlabfig', false, 'savejpeg', false
+        titls_array, {},...
+        'SimMode', 'meanstd', 'separateExpSim', true,...
+        'savematlabfig', figsave, 'savejpeg', jpgsave,...
+        'projdir', projdir, 'tstamp', ts1,...
+        'extrafignamestring', 'RNAspike');
     
     marrayOrd = mvarray(mi(2).paramMaps(mi(2).orderingIx),:,samplePoints);
+    titls = arrayfun(@num2str, mi(2).dosedVals, 'UniformOutput', false);
+    titls_array = cell(length(titls), 2, length(mi(2).measuredSpeciesIndex));
+    ms = {'MG aptamer', 'deGFP'};
+    for i = 1:length(mi(2).measuredSpeciesIndex)
+        for j = 1:length(titls)
+            titls_array(j, 1, i) = {[ms{i} ', ' titls{j} 'nM initial DNA, Exp data']};
+            titls_array(j, 2, i) = {[ms{i} ', ' titls{j} 'nM initial DNA, MCMC samples']};
+        end
+    end
     mcmc_trajectories(mi(2).emo, data_info(mi(2).dataToMapTo), mi(2), marrayOrd,...
         titls_array, {},...
-        'SimMode', 'meanstd');
+        'SimMode', 'meanstd', 'separateExpSim', true,...
+        'savematlabfig', figsave, 'savejpeg', jpgsave,...
+        'projdir', projdir, 'tstamp', ts1,...
+        'extrafignamestring', 'MGa_deGFP');
 end
 marrayOrd(:,1:5,end)
 % flagz = ones(26, 1);
--- mcmc_simbio/projects/analysis_regen_G.m
+++ mcmc_simbio/projects/analysis_regen_G.m
@@ -12,6 +12,9 @@
 projdir = [pwd '/mcmc_simbio/projects/proj_acs_dsg2014_regen_G'];
 addpath(projdir)
 
+jpgsave = true;
+figsave = true;
+
 % Load model, mcmc_info, and data_info.
 mobj = model_dsg2014_regen;
 mcmc_info = mcmc_info_dsg2014_regen_G(mobj);
@@ -26,11 +29,12 @@ tsIDtouse = 1;
 plotflag = true;
 switch tsIDtouse
     case 1 
-        ts1temp = '20190209_191801_1_127';
-        ts2temp = '20190210_030032_1_127';
-        
-        nIterID = {1:20 1:11};
-        tstamp = {ts1temp ts2temp};
+        ts1 = '20190209_191801_1_127';
+        ts2 = '20190210_030032_1_127';
+        ts3 = '20190210_123436_1_32';
+        ts4 = '20190210_185320_1_3';
+        nIterID = {1:20 1:29 1:28 1:80};
+        tstamp = {ts1 ts2 ts3};
         load([projdir '/simdata_' ts1temp '/full_variable_set_' ts1temp '.mat'], ...
             'mi',...
             'mcmc_info', 'data_info', 'mai', 'ri');
@@ -41,7 +45,7 @@ mai.masterVector
 
 
 marray_full = mcmc_get_walkers(tstamp,nIterID, projdir);
-marray = marray_full(:, :, 1000:end);
+marray = marray_full(:, :, 1:end);
 parnames = ...
     [{'TX_{cat}'    }
     {'pol_{Kd}'     }
@@ -54,12 +58,16 @@ parnames = ...
 if plotflag
     close all
     % Plot trace and corner (posterior distribution) plots
-mcmc_plot(marray(:, 1:50:end,:), parnames(:));
-     mcmc_plot(marray(1:6, 1:20:end,1:end), parnames(1:6));
+mcmc_plot(marray(:, 1:10:end,1:50:end), parnames(:),...
+    'savematlabfig', figsave, 'savejpeg', jpgsave,...
+    'projdir', projdir, 'tstamp', ts1, 'extrafignamestring', 'WithTransient');
+mcmc_plot(marray(:, 1:3:end,(120*50):50:end), parnames(:),...
+    'savematlabfig', figsave, 'savejpeg', jpgsave,...
+    'projdir', projdir, 'tstamp', ts1, 'extrafignamestring', 'BurnedIn');
 %     mcmc_plot(marray(6+(1:8), 1:20:end,1:end), parnames(6+(1:8)));
 
     figure
-    [C,lags,ESS]=eacorr(marray(:, :,:));%10000:end
+    [C,lags,ESS]=eacorr(marray(:, :,(120*50):end));%10000:end
     plot(lags,C,'.-',lags([1 end]),[0 0],'k');
     grid on
     xlabel('lags')
@@ -70,17 +78,19 @@ mcmc_plot(marray(:, 1:50:end,:), parnames(:));
     %
     titls = arrayfun(@num2str, mi(1).dosedVals, 'UniformOutput', false);
     titls_array = cell(length(titls), 1, length(mi(1).measuredSpeciesIndex));
+    ms = {'MG aptamer', 'deGFP'};
     for i = 1:length(mi(1).measuredSpeciesIndex)
         for j = 1:length(titls)
-            titls_array(j, 1, i) = titls(j);
+            titls_array(j, 1, i) = {[ms{i} ', ' titls{j} 'nM initial DNA, Exp data']};
+            titls_array(j, 2, i) = {[ms{i} ', ' titls{j} 'nM initial DNA, MCMC samples']};
         end
     end
     mvarray = masterVecArray(marray, mai);
-    samplePoints = ceil(size(mvarray, 3) * [.8, 1]);
+    samplePoints = ceil(size(mvarray, 3) * [.9, 1]);
     marrayOrd = mvarray(mi(1).paramMaps(mi(1).orderingIx),:,samplePoints);
     mcmc_trajectories(mi(1).emo, data_info(mi(1).dataToMapTo), mi(1), marrayOrd,...
         titls_array, {},...
-        'SimMode', 'meanstd');
+        'SimMode', 'meanstd', 'separateExpSim', true);
 end
 marrayOrd(:,1:5,end)
 % flagz = ones(26, 1);
