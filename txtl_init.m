8ce9a1ab4f185ae6c3761560fbc3371d82a42c6f
Vipul <vipulsinghal02@gmail.com>
2020-05-13 21:02:42 +0800

    windiows compatible initialization.

--- txtl_init.m
+++ txtl_init.m
@@ -1,18 +1,39 @@
 function txtldir = txtl_init
-fp = mfilename('fullpath'); 
-slashes = regexp(fp, '/');
-filedir = fp(1:slashes(end)-1);
-addpath(filedir);
-addpath(genpath([filedir '/auxiliary']))
-addpath([filedir '/components'])
-addpath([filedir '/config'])
-addpath([filedir '/core'])
-addpath([filedir '/examples'])
-addpath([filedir '/examples/CompanionFiles'])
-addpath([filedir '/tests'])
-addpath([filedir '/data'])
-addpath([filedir '/mcmc_simbio'])
-addpath([filedir '/generate_paper_figs'])
-
-txtldir = filedir;
+if ispc
+    
+    fp = mfilename('fullpath');
+    slashes = regexp(fp, '\');
+    filedir = fp(1:slashes(end)-1);
+    addpath(filedir);
+    addpath(genpath([filedir '\auxiliary']))
+    addpath([filedir '\components'])
+    addpath([filedir '\config'])
+    addpath([filedir '\core'])
+    addpath([filedir '\examples'])
+    addpath([filedir '\examples\CompanionFiles'])
+    addpath([filedir '\tests'])
+    addpath([filedir '\data'])
+    addpath([filedir '\mcmc_simbio'])
+    addpath([filedir '\generate_paper_figs'])
+    
+    txtldir = filedir;
+else
+    
+    fp = mfilename('fullpath');
+    slashes = regexp(fp, '/');
+    filedir = fp(1:slashes(end)-1);
+    addpath(filedir);
+    addpath(genpath([filedir '/auxiliary']))
+    addpath([filedir '/components'])
+    addpath([filedir '/config'])
+    addpath([filedir '/core'])
+    addpath([filedir '/examples'])
+    addpath([filedir '/examples/CompanionFiles'])
+    addpath([filedir '/tests'])
+    addpath([filedir '/data'])
+    addpath([filedir '/mcmc_simbio'])
+    addpath([filedir '/generate_paper_figs'])
+    
+    txtldir = filedir;
+end
 end
